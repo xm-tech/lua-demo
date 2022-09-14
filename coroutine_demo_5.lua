5b5f502480df88f9fc269a71b57ec0e6b76746e5
xm-tech <maxm09@163.com>
2022-09-14 09:32:09 +0800

    fix comment

--- coroutine_demo_5.lua
+++ coroutine_demo_5.lua
@@ -16,7 +16,7 @@ function producer()
 	end)
 end
 
--- 此处 filter 也是1 协同程序，负责 包装过滤从 prod 收到的数据
+-- 此处 filter 也是1 协同程序，负责 包装过滤从 prod 收到的数据, 并真正发给消费者, 有点类似 linux pipe 的角色, 并且协同程序的切换是相当轻量级的，此处的消耗几乎和函数调用一样
 function filter(prod)
 	return coroutine.create(function ()
 		local line = 1
