312b6c27231c6b93def0eaf3da87bfc18b210990
xm-tech <maxm09@163.com>
2022-09-16 06:38:22 +0800

    fix the comment

--- coroutine_demo_2.lua
+++ coroutine_demo_2.lua
@@ -1,15 +1,17 @@
 function foo(a)
 	print("foo,a=", a)
 	assert(a == 2 , "a is not 2")
-	-- return 2 * a to the main coroutin
+	-- return 2 * a to the main coroutine
 	return coroutine.yield(2 * a)
 end
 
+-- the coroutine c now was at status : yield
 local c = coroutine.create(function (a, b)
 	print("co.1,a="..a..",b="..b)
 	foo(a + 1)
-	-- return a+b , a-b to the caller
+	-- return a+b , a-b to the caller, while the r, s will be the params from the next resume call
 	local r, s = coroutine.yield(a + b, a - b)
+	-- (r,s) == ("lua", "rustt")
 	print("co.2",r,s)
 	-- return b, "end" to the caller
 	return b, "end"
@@ -21,4 +23,4 @@ print("main.1", coroutine.resume(c, 1, 2))
 -- will get true, 3 , -1
 print("main.2", coroutine.resume(c, 10))
 -- will get true, 2, end
-print("main.3", coroutine.resume(c, "lua", "rust"))
+print("main.3", coroutine.resume(c, "lua", "rustt"))
