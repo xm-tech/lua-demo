9ee2ca981f8bf4cf76ebe9d4fc8d9396ccf82161
xm-tech <maxm09@163.com>
2022-11-26 07:53:27 +0800

    script args

--- varargs_demo.lua
+++ varargs_demo.lua
@@ -2,8 +2,10 @@
 	lua 可变参数 demo
 ]]
 
+-- 接受传给本脚本的调用参数
+local cmd_args = {...}
 
-local function eatFruit(person, ...)
+local function eat_fruit(person, ...)
 	local args = {...}
 	print(person, "u can eat these fruits:")
 	for _, fruit in pairs(args) do
@@ -11,4 +13,9 @@ local function eatFruit(person, ...)
 	end
 end
 
-eatFruit("vimer", "apple", "orange")
+eat_fruit("vimer", "apple", "orange")
+
+print("cmd_args:")
+for _, parg in pairs(cmd_args) do
+	print(parg)
+end
