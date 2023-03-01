e79e9cfba34e49959a801d57e0099ab3c8a058f5
xm-tech <maxm09@163.com>
2023-03-01 13:31:32 +0800

    familiar with coroutine yield

--- coroutine_demo_2.lua
+++ coroutine_demo_2.lua
@@ -18,6 +18,7 @@ local c = coroutine.create(function (a, b)
 end)
 
 -- resume 和 yield 的配合强大之处在于，resume 处于主线程中，它将外部状态（数据）传入到协同程序内部；而 yield 则将内部的状态（数据）返回到主线程中。
+-- 也即 resume 和 yield 配合， 可以拿回函数内部任何位置的状态
 -- will get true, 4
 print("main.1", coroutine.resume(c, 1, 2))
 -- will get true, 3 , -1
